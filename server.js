61a45f1c8ac7763002a4043c2a3e2b147dbb8ef7
Kriston Burnstein <kriston.burnstein@gmail.com>
2022-12-06 19:57:55 -0500

    Add form to index.html and body-parser to server.js

--- server.js
+++ server.js
@@ -3,12 +3,20 @@ console.log(__dirname)
 
 const { application } = require('express')
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true}))
+//url encoded method with body-parser tells body-parser to extract data from the form element and add it to the body property in the request object
+//this would allow us to see values from the form elemenmt inside req.body now
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
-
+app.post('/quotes', (req, res) => {
+    console.log(req.body)
+})
 
 
 app.listen(2525, function() {
